dc3729d9882200dc741e8c9d8371ab0aaba57474
Marco Osorio <marco.osorio86@hotmail.com>
2023-01-18 16:36:42 -0500

    corrección en el reader

--- csvpractice/reader.py
+++ csvpractice/reader.py
@@ -2,7 +2,7 @@ import csv
 
 def read_csv(path):
     with open(path, 'r') as csvfile: 
-        reader = csv.reader(csvfile, delimiter=';') 
+        reader = csv.reader(csvfile, delimiter=',') 
         header = next(reader) 
         data = []
         for row in reader: 
